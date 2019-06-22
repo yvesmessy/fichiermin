0700322b2378d83a7e6280fbbc059fec371a3425
Yves Messy <yves.messy@outlook.com>
2019-06-22 16:30:24 +0100

    specified a gitignore file to ignore pom and build data or files produced in the process

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+/* Author: Yves Guillaume A Messy 
+ *Basic Maven+Java project build example. Gradle version to come
+ * */
+
+public class HelloWorld {
+
+    // method main(): ALWAYS the APPLICATION entry point
+    public static void main (String[] args) {
+		System.out.println ("This is an initial Maven file with Java source code. Consider this project boilerplate ");
+    }
+}
